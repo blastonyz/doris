030d255622dc8e368e5a898a8313a65e40e58527
blastonyz <blastonyzamora@gmail.com>
2024-08-08 18:43:24 -0300

    script async

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
-
+import Head from 'next/head';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-  
-  <script type="module" src="https://unpkg.com/@splinetool/viewer@1.9.5/build/spline-viewer.js"></script>
+  <Head>
+    <script type="module" src="https://unpkg.com/@splinetool/viewer@1.9.5/build/spline-viewer.js" async></script>
+
+  </Head>
   <spline-viewer url="https://prod.spline.design/Ovy9qXJefuldPXIh/scene.splinecode"></spline-viewer>
     </main>
   );
